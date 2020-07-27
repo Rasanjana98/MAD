3301fa13cb4e28fccea9e597b22c48e62c387084
Rasanjana98 <kuruppu.rasanjana98@gmail.com>
2020-07-27 15:20:15 +0530

    log class part done

--- app/src/main/java/com/rasanjana/usingstrings/ActivityOne.java
+++ app/src/main/java/com/rasanjana/usingstrings/ActivityOne.java
@@ -14,8 +14,34 @@ public class ActivityOne extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Log.i();
+        Log.i("LifeCycle","onCreate() invoked");
+
         txtView2=findViewById(R.id.textView2);
         txtView2.setText(R.string.Msg2);
     }
+    public void onStart(){
+        super.onStart();
+        Log.i("Lifecycle","onStart() invoked");
+    }
+    public void onRestart(){
+        super.onRestart();
+        Log.i("Lifecycle","onRestart() invoked");
+    }
+    public void onResume(){
+        super.onResume();
+        Log.i("Lifecycle","onResume() invoked");
+    }
+    public void onPause(){
+        super.onPause();
+        Log.i("Lifecycle","onPause() invoked");
+    }
+    public void onStop(){
+        super.onStop();
+        Log.i("Lifecycle","onStop() invoked");
+    }
+    public void onDestroy(){
+        super.onDestroy();
+        Log.i("Lifecycle","onDestroy() invoked");
+    }
+
 }
